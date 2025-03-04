586bf688be1c7eea56af5d606fd3a81b2e12ca82
Dor Sam <dor.samoha@gmail.com>
2025-03-04 21:16:04 +0200

    Initial Commit

---
+++ BitcoinMempool/BitcoinMempoolApp.swift
@@ -0,0 +1,17 @@
+//
+//  BitcoinMempoolApp.swift
+//  BitcoinMempool
+//
+//  Created by Dor Sam on 04/03/2025.
+//
+
+import SwiftUI
+
+@main
+struct BitcoinMempoolApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
